61bd16c2ca28a677bb7666928540d30fc1c99fc4
İbrahim EDİZ <edizdesign@gmail.com>
2022-01-22 08:32:46 +0000

    22012022_env

---
+++ fatihcan/app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from routes import routes
+
+app = Flask(__name__)
--- hasan/app/__init__.py
+++ hasan/app/__init__.py
@@ -1,1 +1,6 @@
-[A[B[A
+# package importing
+from flask import Flask
+from app import route
+
+# initialize app obj
+app=Flask(__name__)
